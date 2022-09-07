00a8fde1debf29ce7abea4442c8272073fa73e25
Ali Amjad <66186390+Ali-A-Koye@users.noreply.github.com>
2022-09-08 02:00:26 +0300

    complete route functionality

--- src/routes/thumbnail.route.ts
+++ src/routes/thumbnail.route.ts
@@ -1,15 +1,11 @@
 import { FastifyPluginAsync } from "fastify";
 
 const thumbnail: FastifyPluginAsync = async (fastify) => {
-  fastify.post("/thumbnail", fastify.schema.thumbnail.thumbnailPostSchema , async function () {
-    return "thumbnail";
-  });
-
-  fastify.get("/thumbnail/:id",fastify.schema.thumbnail.thumbnailGetSchema , async (req, reply): Promise<any> => {
-    reply.code(200).send({
-      result: "thumbnail 3",
-    });
-  });
+  fastify.post(
+    "/thumbnail",
+    fastify.schema.thumbnail.thumbnailPostSchema,
+    fastify.controller.thumbnail.postThumbnail
+  );
 };
 
 export default thumbnail;
