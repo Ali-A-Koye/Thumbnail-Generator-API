bea289105327875e76b1f6f79132c239d73e693a
Ali Amjad <66186390+Ali-A-Koye@users.noreply.github.com>
2022-09-12 21:44:28 +0300

    Handler Uploads and Creates a Session for the incoming Image

--- src/handlers/uploader.ts
+++ src/handlers/uploader.ts
@@ -1,5 +1,16 @@
-const handler = (msg: any) => {
-    console.log(msg.content, "received from uploader handler");
+import { Message } from "amqplib";
+import { promises as fs } from 'fs';
+import { v4 as uuidv4 } from 'uuid';
+
+const handler = async (msg: Message) => {
+    const fileSuffix:string = "jpg"
+    const folderDir:string = uuidv4();
+    await fs.mkdir(`${__dirname}/../../uploads/${folderDir}`);
+    const image:string= `${__dirname}/../../uploads/${folderDir}/${uuidv4()}.${fileSuffix}`;
+    await fs.writeFile(`${image}`, msg.content);
+
+    // channel.ack(msg);
+
 }
 
 
